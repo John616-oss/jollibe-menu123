5b85e20a9bbea64de3b21f17593ddb66aeaefd22
John Carlo S. Verzosa <amaldaverzosa3@gmail.com>
2026-04-28 15:53:37 +0800

    Add project files.

---
+++ jollibe menu/admin.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace jollibe_menu
+{
+    partial class admin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(admin));
+            panel1 = new Panel();
+            label2 = new Label();
+            panel2 = new Panel();
+            btnLog = new Button();
+            label1 = new Label();
+            pictureBox1 = new PictureBox();
+            btnStocks = new Button();
+            btnEmployee = new Button();
+            dataGridView1 = new DataGridView();
+            txtSearch = new TextBox();
+            button1 = new Button();
+            panel3 = new Panel();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            label3 = new Label();
+            button3 = new Button();
+            button2 = new Button();
+            textBox4 = new TextBox();
+            textBox3 = new TextBox();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            panel1.SuspendLayout();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            panel3.SuspendLayout();
+            SuspendLayout();
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Red;
+            panel1.Controls.Add(label2);
+            panel1.Location = new Point(-5, -5);
+            panel1.Margin = new Padding(3, 2, 3, 2);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(1165, 67);
+            panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Segoe UI Black", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = SystemColors.ButtonHighlight;
+            label2.Location = new Point(515, 18);
+            label2.Name = "label2";
+            label2.Size = new Size(217, 32);
+            label2.TabIndex = 1;
+            label2.Text = "admin dashboard";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = SystemColors.ControlLightLight;
+            panel2.Controls.Add(btnLog);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox1);
+            panel2.Controls.Add(btnStocks);
+            panel2.Controls.Add(btnEmployee);
+            panel2.Location = new Point(-5, 61);
+            panel2.Margin = new Padding(3, 2, 3, 2);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(224, 475);
+            panel2.TabIndex = 1;
+            // 
+            // btnLog
+            // 
+            btnLog.BackColor = Color.Red;
+            btnLog.FlatStyle = FlatStyle.Popup;
+            btnLog.ForeColor = SystemColors.ButtonFace;
+            btnLog.Location = new Point(21, 355);
+            btnLog.Margin = new Padding(3, 2, 3, 2);
+            btnLog.Name = "btnLog";
+            btnLog.Size = new Size(173, 40);
+            btnLog.TabIndex = 4;
+            btnLog.Text = "LOGOUT";
+            btnLog.UseVisualStyleBackColor = false;
+            btnLog.Click += btnLog_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Microsoft Sans Serif", 13.8F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label1.Location = new Point(16, 14);
+            label1.Name = "label1";
+            label1.Size = new Size(126, 24);
+            label1.TabIndex = 2;
+            label1.Text = "ADMIN USER";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = Color.Transparent;
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox1.Location = new Point(21, 52);
+            pictureBox1.Margin = new Padding(3, 2, 3, 2);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(177, 95);
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
+            // 
+            // btnStocks
+            // 
+            btnStocks.BackColor = Color.Red;
+            btnStocks.FlatStyle = FlatStyle.Popup;
+            btnStocks.ForeColor = SystemColors.ButtonFace;
+            btnStocks.Location = new Point(21, 279);
+            btnStocks.Margin = new Padding(3, 2, 3, 2);
+            btnStocks.Name = "btnStocks";
+            btnStocks.Size = new Size(173, 40);
+            btnStocks.TabIndex = 3;
+            btnStocks.Text = "STOCKS";
+            btnStocks.UseVisualStyleBackColor = false;
+            // 
+            // btnEmployee
+            // 
+            btnEmployee.BackColor = Color.Red;
+            btnEmployee.FlatStyle = FlatStyle.Popup;
+            btnEmployee.ForeColor = SystemColors.ButtonFace;
+            btnEmployee.Location = new Point(21, 200);
+            btnEmployee.Margin = new Padding(3, 2, 3, 2);
+            btnEmployee.Name = "btnEmployee";
+            btnEmployee.Size = new Size(173, 40);
+            btnEmployee.TabIndex = 2;
+            btnEmployee.Text = "Employee";
+            btnEmployee.UseVisualStyleBackColor = false;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(217, 113);
+            dataGridView1.Margin = new Padding(3, 2, 3, 2);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowHeadersWidth = 51;
+            dataGridView1.Size = new Size(580, 423);
+            dataGridView1.TabIndex = 2;
+            // 
+            // txtSearch
+            // 
+            txtSearch.Location = new Point(492, 85);
+            txtSearch.Margin = new Padding(3, 2, 3, 2);
+            txtSearch.Name = "txtSearch";
+            txtSearch.PlaceholderText = "Search here";
+            txtSearch.Size = new Size(196, 23);
+            txtSearch.TabIndex = 3;
+            txtSearch.TextChanged += txtSearch_TextChanged;
+            // 
+            // button1
+            // 
+            button1.BackColor = SystemColors.AppWorkspace;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Microsoft YaHei UI", 10.8F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(694, 83);
+            button1.Margin = new Padding(3, 2, 3, 2);
+            button1.Name = "button1";
+            button1.Size = new Size(95, 26);
+            button1.TabIndex = 4;
+            button1.Text = "SEARCH";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.LightSteelBlue;
+            panel3.Controls.Add(label6);
+            panel3.Controls.Add(label5);
+            panel3.Controls.Add(label4);
+            panel3.Controls.Add(label3);
+            panel3.Controls.Add(button3);
+            panel3.Controls.Add(button2);
+            panel3.Controls.Add(textBox4);
+            panel3.Controls.Add(textBox3);
+            panel3.Controls.Add(textBox2);
+            panel3.Controls.Add(textBox1);
+            panel3.Location = new Point(795, 61);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(365, 475);
+            panel3.TabIndex = 5;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(93, 190);
+            label6.Name = "label6";
+            label6.Size = new Size(38, 15);
+            label6.TabIndex = 9;
+            label6.Text = "label6";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(93, 140);
+            label5.Name = "label5";
+            label5.Size = new Size(38, 15);
+            label5.TabIndex = 8;
+            label5.Text = "label5";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(93, 94);
+            label4.Name = "label4";
+            label4.Size = new Size(38, 15);
+            label4.TabIndex = 7;
+            label4.Text = "label4";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(93, 40);
+            label3.Name = "label3";
+            label3.Size = new Size(38, 15);
+            label3.TabIndex = 6;
+            label3.Text = "label3";
+            // 
+            // button3
+            // 
+            button3.Location = new Point(93, 291);
+            button3.Name = "button3";
+            button3.Size = new Size(195, 31);
+            button3.TabIndex = 5;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(93, 247);
+            button2.Name = "button2";
+            button2.Size = new Size(195, 31);
+            button2.TabIndex = 4;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(93, 208);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(195, 23);
+            textBox4.TabIndex = 3;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(93, 158);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(195, 23);
+            textBox3.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(93, 112);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(195, 23);
+            textBox2.TabIndex = 1;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(93, 67);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(195, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // admin
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1155, 541);
+            Controls.Add(panel3);
+            Controls.Add(button1);
+            Controls.Add(txtSearch);
+            Controls.Add(dataGridView1);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Margin = new Padding(3, 2, 3, 2);
+            Name = "admin";
+            Text = "admin";
+            Load += admin_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            panel3.ResumeLayout(false);
+            panel3.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private Panel panel2;
+        private PictureBox pictureBox1;
+        private Button btnStocks;
+        private Button btnEmployee;
+        private Label label1;
+        private Label label2;
+        private DataGridView dataGridView1;
+        private TextBox txtSearch;
+        private Button btnLog;
+        private Button button1;
+        private Panel panel3;
+        private Label label6;
+        private Label label5;
+        private Label label4;
+        private Label label3;
+        private Button button3;
+        private Button button2;
+        private TextBox textBox4;
+        private TextBox textBox3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+    }
+}
---
+++ jollibe menu/admin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace jollibe_menu
+{
+    public partial class admin : Form
+    {
+        public admin()
+        {
+            InitializeComponent();
+        }
+
+        private void admin_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnLog_Click(object sender, EventArgs e)
+        {
+            new Form1().Show();
+            this.Hide();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

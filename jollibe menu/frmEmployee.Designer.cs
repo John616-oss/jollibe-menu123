d13f79a0f34c77a42ecadcfe40c8836fa4a8b4d3
BravePJ <bravepjnavarro31@gmail.com>
2026-04-29 15:39:01 +0800

    iniba ko yongiba

--- jollibe menu/frmEmployee.Designer.cs
+++ jollibe menu/frmEmployee.Designer.cs
@@ -31,6 +31,13 @@
             flowLayoutPanel1 = new FlowLayoutPanel();
             panel1 = new Panel();
             panel2 = new Panel();
+            button1 = new Button();
+            button2 = new Button();
+            dataGridView1 = new DataGridView();
+            textBox1 = new TextBox();
+            button3 = new Button();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
             SuspendLayout();
             // 
             // flowLayoutPanel1
@@ -44,6 +51,8 @@
             // 
             // panel1
             // 
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
             panel1.Dock = DockStyle.Left;
             panel1.Location = new Point(0, 67);
             panel1.Name = "panel1";
@@ -58,17 +67,65 @@
             panel2.Size = new Size(360, 474);
             panel2.TabIndex = 2;
             // 
+            // button1
+            // 
+            button1.Location = new Point(38, 140);
+            button1.Name = "button1";
+            button1.Size = new Size(135, 42);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(38, 220);
+            button2.Name = "button2";
+            button2.Size = new Size(135, 39);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(224, 138);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(565, 403);
+            dataGridView1.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(466, 109);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(207, 23);
+            textBox1.TabIndex = 4;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(689, 109);
+            button3.Name = "button3";
+            button3.Size = new Size(100, 23);
+            button3.TabIndex = 5;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
             // frmEmployee
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(1155, 541);
+            Controls.Add(button3);
+            Controls.Add(textBox1);
+            Controls.Add(dataGridView1);
             Controls.Add(panel2);
             Controls.Add(panel1);
             Controls.Add(flowLayoutPanel1);
             Name = "frmEmployee";
             Text = "frmEmployee";
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
@@ -76,5 +133,10 @@
         private FlowLayoutPanel flowLayoutPanel1;
         private Panel panel1;
         private Panel panel2;
+        private Button button2;
+        private Button button1;
+        private DataGridView dataGridView1;
+        private TextBox textBox1;
+        private Button button3;
     }
 }
